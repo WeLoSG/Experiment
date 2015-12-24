8424a948d7384bad76dd330090a276256e1fb7f5
Jiang Sheng <gisonrg@gmail.com>
2015-12-24 11:37:42 +0800

    Add skeleton for client.

---
+++ client/app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name MyApp.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('MyApp')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
